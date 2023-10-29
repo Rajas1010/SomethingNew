8d6bfc7de71d6b6838f1300392ffb39c3c1bd970
Rajas1010 <Rajasekar.Rj100@gmail.com>
2023-10-29 14:12:23 +0530

    Use underscores for clearer numeral representation

---
+++ UnderscoresInNumerals.java
@@ -0,0 +1,11 @@
+public class UnderscoresInNumerals 
+{
+	public static void main(String args[])
+	{
+		long forLong = 1_183_548_876_845L;
+		double forDecimal = 1.546_674_876;
+		System.out.println(forLong);
+		System.out.println(forDecimal);
+	}
+}
+
