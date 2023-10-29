60f6469dfb285c44af389cc6f0f2cfc4b47dc7d0
Rajas1010 <Rajasekar.Rj100@gmail.com>
2023-10-29 13:53:30 +0530

    useDifferentFileAndClassName

---
+++ DifferentFileAndClassName.java
@@ -0,0 +1,13 @@
+class DifferentClassName
+{
+	public static void main(String args[])
+	{
+		System.out.println("HelloWorld");
+	}
+}
+
+//Note to run
+/*
+# javac DifferentFileAndClassName.java
+# java DifferentClassName
+*/
